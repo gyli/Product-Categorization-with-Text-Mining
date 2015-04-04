bf0699ec1caeb968029bc220d970555ec136d45d
Guangyang Li <mail@guangyangli.com>
2015-04-04 04:03:04 -0400

    Save the model into local files

--- 2-text-cleaning.py
+++ 2-text-cleaning.py
@@ -3,14 +3,16 @@
 
 # Download the NLTK resource
 from DictMySQLdb import DictMySQLdb
+from gensim import corpora, models, similarities
 import nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 import pandas as pd
+import pickle
 
 
 # Get the production title from database
-nltk.download()
+# nltk.download()
 
 # Database connection
 pd_db = DictMySQLdb(user='root', passwd='', host='127.0.0.1', db='product')
@@ -20,8 +22,58 @@ product = pd_db.select(tablename='item', field=['title', 'root_category_id'])
 # Convert to pandas DataFrame
 product = pd.DataFrame([{'title': i[0], 'cat': i[1]} for i in product])
 
+# Get root categories
+category = pd_db.select(tablename='category', field=['id', 'name'], value={'upper_level': 1})
+category = pd.DataFrame([{'cat': i[0], 'cat_name': i[1]} for i in category])
+
+product_category = product.merge(category, on='cat', how='left')
+
 # Remove stopwords and stemming
 stopwords = nltk.corpus.stopwords.words("english")
 porter_stemmer = PorterStemmer()
 tokenizer = RegexpTokenizer(r'\w+')
 product['cleaned_title'] = product['title'].map(lambda row: tokenizer.tokenize(' '.join([porter_stemmer.stem(word.lower()) for word in row.split() if word not in stopwords])))
+
+# Build dictionary
+dictionary = corpora.Dictionary(product['cleaned_title'])
+
+# Translate the documents into vectors based on the dictionary
+corpus = [dictionary.doc2bow(text) for text in product['cleaned_title']]
+
+# Build TF-IDF model
+tfidf = models.TfidfModel(corpus)
+
+# Calculate the TF-IDF value of the documents
+corpus_tfidf = tfidf[corpus]
+
+# Build LSI model for dimentional reduction
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)
+lsi.print_topics(100)
+
+# Get the text features of original documents
+corpus_lsi = lsi[corpus_tfidf]
+
+# Build the index of the similarity
+index = similarities.MatrixSimilarity(lsi[corpus])
+
+# Save the LSI model and the similarity matrix
+pickle.dump(lsi, open("lsi.obj", "w"))
+pickle.dump(index, open("ind.obj", "w"))
+pickle.dump(dictionary, open("dictionary.obj", "w"))
+pickle.dump(product_category['cat_name'], open("cat_list.obj", "w"))
+
+# Input the new text
+query = "iPhone 6 Plus"
+
+# Run the same text processing for the input text
+query_bow = dictionary.doc2bow(tokenizer.tokenize(' '.join([porter_stemmer.stem(word.lower()) for word in query.split() if word not in stopwords])))
+
+# Translate the text into the space we build with LSI model
+query_lsi = lsi[query_bow]
+
+# Calculate the text simimarity amoung all the documents
+sims = index[query_lsi]
+
+# Sort the result
+sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
+print sort_sims[0:5]
--- 3-Analysis.py
+++ 3-Analysis.py
@@ -1,33 +1,163 @@
 #!/usr/bin/python
 # -*-coding:UTF-8 -*-
 
-from gensim import corpora, models, similarities
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+import pickle
+import sys
 
 
+lsi = pickle.load(open("lsi.obj", "r"))
+index = pickle.load(open("ind.obj", "r"))
+dictionary = pickle.load(open("dictionary.obj", "r"))
+cat_list = pickle.load(open("cat_list.obj", "r"))
 
-dictionary = corpora.Dictionary(product['title'])
+# Input the new text
+query = "Apple MacBook Air MJVE2LL/A 13.3-Inch Laptop (128 GB) NEWEST VERSION"
+query = sys.argv[1]
 
-corpus = [dictionary.doc2bow(text) for text in product['title']]
+tokenizer = RegexpTokenizer(r'\w+')
+porter_stemmer = PorterStemmer()
+stopwords = [u'i',
+             u'me',
+             u'my',
+             u'myself',
+             u'we',
+             u'our',
+             u'ours',
+             u'ourselves',
+             u'you',
+             u'your',
+             u'yours',
+             u'yourself',
+             u'yourselves',
+             u'he',
+             u'him',
+             u'his',
+             u'himself',
+             u'she',
+             u'her',
+             u'hers',
+             u'herself',
+             u'it',
+             u'its',
+             u'itself',
+             u'they',
+             u'them',
+             u'their',
+             u'theirs',
+             u'themselves',
+             u'what',
+             u'which',
+             u'who',
+             u'whom',
+             u'this',
+             u'that',
+             u'these',
+             u'those',
+             u'am',
+             u'is',
+             u'are',
+             u'was',
+             u'were',
+             u'be',
+             u'been',
+             u'being',
+             u'have',
+             u'has',
+             u'had',
+             u'having',
+             u'do',
+             u'does',
+             u'did',
+             u'doing',
+             u'a',
+             u'an',
+             u'the',
+             u'and',
+             u'but',
+             u'if',
+             u'or',
+             u'because',
+             u'as',
+             u'until',
+             u'while',
+             u'of',
+             u'at',
+             u'by',
+             u'for',
+             u'with',
+             u'about',
+             u'against',
+             u'between',
+             u'into',
+             u'through',
+             u'during',
+             u'before',
+             u'after',
+             u'above',
+             u'below',
+             u'to',
+             u'from',
+             u'up',
+             u'down',
+             u'in',
+             u'out',
+             u'on',
+             u'off',
+             u'over',
+             u'under',
+             u'again',
+             u'further',
+             u'then',
+             u'once',
+             u'here',
+             u'there',
+             u'when',
+             u'where',
+             u'why',
+             u'how',
+             u'all',
+             u'any',
+             u'both',
+             u'each',
+             u'few',
+             u'more',
+             u'most',
+             u'other',
+             u'some',
+             u'such',
+             u'no',
+             u'nor',
+             u'not',
+             u'only',
+             u'own',
+             u'same',
+             u'so',
+             u'than',
+             u'too',
+             u'very',
+             u's',
+             u't',
+             u'can',
+             u'will',
+             u'just',
+             u'don',
+             u'should',
+             u'now']
 
-tfidf = models.TfidfModel(corpus)
+# Run the same text processing for the input text
+query_bow = dictionary.doc2bow(tokenizer.tokenize(
+    ' '.join([porter_stemmer.stem(word.lower()) for word in query.split() if word not in stopwords])))
 
-corpus_tfidf = tfidf[corpus]
-
-
-lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)
-lsi.print_topics(100)
-
-corpus_lsi = lsi[corpus_tfidf]
-
-index = similarities.MatrixSimilarity(lsi[corpus])
-
-
-
-query = "iPhone 6 Plus"
-query_bow = dictionary.doc2bow(tokenizer.tokenize(' '.join([porter_stemmer.stem(word.lower()) for word in query.split() if word not in stopwords])))
+# Translate the text into the space we build with LSI model
 query_lsi = lsi[query_bow]
-sims = index[query_lsi]
-sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
-sort_sims[0:5]
 
-product.iloc[25535]
+# Calculate the text simimarity amoung all the documents
+sims = index[query_lsi]
+
+# Sort the result
+sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
+
+print cat_list[[i[0] for i in sort_sims[0:10]]]
+print cat_list[[i[0] for i in sort_sims[0:5]]].value_counts().index[0]
